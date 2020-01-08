34362e8dd2cb9bc678166e09b7419a3d7b3a4c81
dandumitriu33 <dandumitriu33@gmail.com>
2020-01-08 13:30:39 +0200

    Add the database_connector module

--- server.py
+++ server.py
@@ -7,5 +7,10 @@ def home():
     return render_template('home.html')
 
 
+@app.applicants('/applicants')
+def all_applicants_first_name():
+    return render_template('applicants.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
